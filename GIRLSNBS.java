6b38070452e75a4b73c93efc9291c45cc1ca5e1c
Rohit <rohitjain18005@gmail.com>
2018-10-12 20:16:39 +0530

    GIRLSNBS completed

--- GIRLSNBS.java
+++ GIRLSNBS.java
@@ -1,5 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class GIRLSNBS {
+
+  public static void main(String args[]) {
+    Scanner st = new Scanner(System.in);
+    int G = st.nextInt();
+    int B = st.nextInt();
+    int x,y;
+    while (G != -1 && B != -1) {
+      if (G == B) {
+        if (G == 0) {
+          System.out.println(0);
+        } else {
+          System.out.println(1);
+          }
+        } else {
+        if(G > B) {
+          x = G;
+          y = B;
+        } else {
+          x = B;
+          y = G;
+        }
+        if((x%(y+1))==0) {
+          System.out.println(x/(y+1));
+        } else {
+          System.out.println(x/(y+1)+1);
+        }
+      }
+      G = st.nextInt();
+      B = st.nextInt();
+    }
+  }
 
 }
